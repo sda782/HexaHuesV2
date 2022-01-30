4f861bd1d4c7a8779abf3c7a0f3e40792373a730
sda782 <saerdna.petersen@gmail.com>
2022-01-30 15:45:46 +0100

    init project

---
+++ HexaHues/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
